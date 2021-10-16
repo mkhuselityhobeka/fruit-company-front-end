e388b8df5484465afe8c5610446bff5fd8b6de23
mkhuselityhobeka@gmail.com <mkhuselityhobeka@gmail.com>
2021-10-16 10:10:10 +0200

    created products component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponentComponent } from './home-component/home-component.component';
 import { UserLoginComponentComponent } from './user-login-component/user-login-component.component';
 import { UserRegistrationComponentComponent } from './user-registration-component/user-registration-component.component';
+import { ProductComponentComponent } from './product-component/product-component.component';
 
 
 const routes: Routes = [
   {path:'', redirectTo:"home", pathMatch:'full'},
   {path:'home', component:HomeComponentComponent},
+  {path:'products', component:ProductComponentComponent},
   {path:'login', component:UserLoginComponentComponent},
   {path:'registration', component:UserRegistrationComponentComponent}
 ];
