63b884549f2fce940bac2cc3a9e68827e14942e6
mkhuselityhobeka@gmail.com <mkhuselityhobeka@gmail.com>
2021-10-16 12:58:30 +0200

    added product list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,17 @@ import { HomeComponentComponent } from './home-component/home-component.componen
 import { UserLoginComponentComponent } from './user-login-component/user-login-component.component';
 import { UserRegistrationComponentComponent } from './user-registration-component/user-registration-component.component';
 import { ProductComponentComponent } from './product-component/product-component.component';
-
+import { ProductListComponentComponent } from './product-list-component/product-list-component.component';
 
 const routes: Routes = [
   {path:'', redirectTo:"home", pathMatch:'full'},
   {path:'home', component:HomeComponentComponent},
-  {path:'products', component:ProductComponentComponent},
+  {path:'add-products', component:ProductComponentComponent},
+  {path:'product-list' , component:ProductListComponentComponent},
   {path:'login', component:UserLoginComponentComponent},
   {path:'registration', component:UserRegistrationComponentComponent}
+  
+  
 ];
 
 @NgModule({
